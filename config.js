a445ce89ac292858cbd2ad07bbe4c47415edc4a8
bibeaK99 <153068885+bibeaK99@users.noreply.github.com>
2023-12-06 15:11:04 +0530

    Update config.js
    Yes

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['9339449049', 'CM Don²⁰²³', true],
+  ['9339449049', 'CM Don²⁰²³', true], 
   [''] 
 ] //Number of owners
 
